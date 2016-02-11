8a0c78d8809e704cc6ace747156248ecc0710399
aleksjp <alexjptr@gmail.com>
2016-02-11 02:19:32 +0200

    Update README

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
 # ric_mod 
 
-Android loadable kernel module that disables Sony RIC write protection on the system partition of Sony Xperia M2 devices.
+Android loadable kernel module that disables Sony RIC write protection on the system partition of Xperia M2 devices.
 
-==========
+**Prerequisites:**  
+Xperia M2 smartphone.  
+Latest firmware(18.6.A.0.182).  
+The kernel sources are published on the [SonyXperiaDev](https://github.com/sonyxperiadev/kernel-copyleft) or can be downloaded from the copyleft [archives](http://dl-developer.sonymobile.com/code/copylefts/18.6.A.0.182.tar.bz2).  
+Cross compiler toolchain for ARM processors.  
 
-Inspired from [wp_mod](https://github.com/flar2/wp_mod)
+**How to Build:**  
+Edit Makefile and change:  
+`KERNEL_BUILD :=` _< kernel source directory >_  
+`KERNEL_CROSS_COMPILE :=` _< your toolchain >_  
+Compile using: ``make`` 
 
+**How to Install and load:**  
+`adb push ric_mod.ko /data/local/tmp/ric_mod.ko`  
+`$ adb shell`  
+`$ su`  
+ `# chmod 0644 /data/local/tmp/ric_mod.ko`  
+`# insmod /data/local/tmp/ric_mod.ko`  
+
+
+**How to Remove:**  
+`$ adb shell`  
+`$ su`   
+`# mount -o remount,rw /system`  
+`# rmmod ric_mod.ko`  
+`# rm -rf /system/lib/modules/ric_mod.ko`
+
+Flashable zip can be download from [here](http://d-h.st/m75F):
+
+Inspired from [wp_mod](https://github.com/flar2/wp_mod).
--- ric_mod.c
+++ ric_mod.c
@@ -3,7 +3,6 @@
 #include <linux/kernel.h>
 #include <linux/kallsyms.h>
 #include <linux/version.h>
-#include <linux/vmalloc.h>
 #include <linux/slab.h>
 #include <asm/mmu_writeable.h>
 
@@ -12,6 +11,7 @@
 #define DRIVER_VERSION "0.1"
 
 #define OPCODE_SIZE 12 /* ARM hook size is 12 bytes */
+
 
 struct hookdata_t
 {
@@ -25,7 +25,8 @@ struct hookdata_t
 LIST_HEAD(head_obj);
 
 static unsigned long m_address;
-static bool ric_enable;
+static bool ric_enable=false; /* Current status - false if protection is enabled, true disabled. */
+
 
 static int sony_ric_disabled(void)
 {
@@ -43,16 +44,20 @@ inline void arm_poke ( void* p_addr, const char* p_data )
 
 }
 
+/* Store the target function pointer, original code and new code to the linked list. */
 static void ric_save ( void* p_addr,  const unsigned char* p_opcode, const unsigned char* p_data )
 {
 	struct hookdata_t* ps;
 
 	if(p_addr!=NULL && p_opcode!=NULL && p_data!=NULL)
 	{
-		ps = kmalloc(sizeof(*ps), GFP_KERNEL);		
-		if ( ps==NULL )
+		/* allocate the memory for the whole object */
+		ps = kzalloc(sizeof(*ps), GFP_KERNEL);
+		if ( !ps )
+		{
+			pr_err("unable to allocate memory\n");
 			return;
-
+		}
 		ps->paddr = p_addr;
 		memcpy(ps->m_data, p_data, OPCODE_SIZE);
 		memcpy(ps->m_opcode, p_opcode, OPCODE_SIZE);
@@ -60,24 +65,27 @@ static void ric_save ( void* p_addr,  const unsigned char* p_opcode, const unsig
 	}
 }
 
+/* Construct shellcode and rewrite target */
 static void ric_rewrite ( void* p_addr, void* p_data )
 {
-	unsigned char m_opcode[OPCODE_SIZE]= {0};
-	unsigned char m_data[OPCODE_SIZE]= {0};
+	unsigned char m_opcode[OPCODE_SIZE]= {0}; /* local buffer for new code */
+	unsigned char m_data[OPCODE_SIZE]= {0};   /* local buffer to save original code */
 
 	/* ldr pc, [pc, #0]; .long addr; .long addr */
 	memcpy(m_opcode, "\x00\xf0\x9f\xe5\x00\x00\x00\x00\x00\x00\x00\x00", OPCODE_SIZE);
 	*(unsigned long*)&(m_opcode)[4] = (unsigned long)p_data;
 	*(unsigned long*)&(m_opcode)[8] = (unsigned long)p_data;
 
-	memcpy(m_data, p_addr, OPCODE_SIZE);
-	
+	memcpy(m_data, p_addr, OPCODE_SIZE); /* save target pointer */
+
+	/* At this point all function calls to sony_ric_enabled() will redirect to sony_ric_disabled(). */
 	arm_poke(p_addr, m_opcode);
-	
+
 	ric_save(p_addr, m_opcode, m_data);
 
 }
 
+/* Restore original opcodes. */
 static void ric_restore ( void* p_addr )
 {
 	struct hookdata_t* ps;
@@ -87,7 +95,7 @@ static void ric_restore ( void* p_addr )
 	{
 		arm_poke(p_addr, ps->m_data);
 		ric_enable=!ric_enable;
-		list_del( &ps->list);		
+		list_del( &ps->list);
 		kfree(ps);
 		break;
 	}
@@ -96,20 +104,19 @@ static void ric_restore ( void* p_addr )
 static int __init ric_mod_init(void)
 {
 
-	printk(KERN_INFO "[%s] loaded\n", __this_module.name);
-
 	m_address=kallsyms_lookup_name("sony_ric_enabled");
 	if(m_address!=0)
 	{
-		ric_rewrite((void*)m_address, &sony_ric_disabled);		
+		ric_rewrite((void*)m_address, &sony_ric_disabled);
 		ric_enable=!ric_enable;
 	}
 	else
 	{
-		printk(KERN_ERR "[%s] Failed to lockup symbol address\n", __this_module.name);		
-		return 1;
+		printk(KERN_ERR "[%s] Unable to loоckup sony_ric_enabled symbol\n", __this_module.name);
+		return -1;
 	}
 
+	printk(KERN_INFO "[%s] module loaded\n", __this_module.name);
 	printk(KERN_INFO "[%s] Sony RIC is now : %s\n", __this_module.name, ric_enable ?  "disabled" : "enabled");
 
 	return 0;
@@ -118,18 +125,16 @@ static int __init ric_mod_init(void)
 static void __exit ric_mod_exit(void)
 {
 
-	printk(KERN_INFO "[%s] unloaded\n", __this_module.name);
+	if(ric_enable)
+		ric_restore((void*)m_address);
 
-	ric_restore((void*)m_address);
-
+	printk(KERN_INFO "[%s] module unloaded\n", __this_module.name);
 	printk(KERN_INFO "[%s] Sony RIC is now : %s\n", __this_module.name, ric_enable ?  "disabled" : "enabled");
-
 
 }
 
 module_init(ric_mod_init);
 module_exit(ric_mod_exit);
-
 
 MODULE_DESCRIPTION(DRIVER_DESCRIPTION);
 MODULE_VERSION(DRIVER_VERSION);
